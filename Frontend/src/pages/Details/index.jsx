cf6ae99ceb0e02c85a65f65baac0ba758d666106
guilherme <guilhermefalck22@gmail.com>
2024-08-16 13:45:10 -0300

    first commit

---
+++ Frontend/src/pages/Details/index.jsx
@@ -0,0 +1,48 @@
+import { Container, Links, Content } from "./styles.js";
+
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
+import { ButtonText } from "../../components/ButtonText";
+
+export function Details() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quia
+            soluta architecto quam nobis saepe dolore sed consequatur eos
+            voluptatum. Ex velit, possimus cupiditate enim tenetur cumque at
+            libero nostrum.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
+    </Container>
+  );
+}
