84af5ed0565aea74b875380eb51957540cf56273
guilherme <guilhermefalck22@gmail.com>
2024-08-28 11:56:02 -0300

    authenticate user

--- Frontend/src/pages/SingIn/index.jsx
+++ Frontend/src/pages/SingIn/index.jsx
@@ -1,8 +1,8 @@
-import { useContext } from "react";
+import { useState } from "react";
 import { FiMail, FiLock } from "react-icons/fi";
 import { Link } from "react-router-dom";
 
-import { MyContext } from "../../myContext";
+import { useAuth } from "../../hooks/auth";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -10,8 +10,14 @@ import { Button } from "../../components/Button";
 import { Container, Form, Background } from "./styles";
 
 export function SingIn() {
-  const data = useContext(MyContext);
-  console.log("Meu Contexto =>?", data);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
   return (
     <Container>
       <Form>
@@ -20,10 +26,20 @@ export function SingIn() {
 
         <h2>Faça seu login</h2>
 
-        <Input placeholder="E-mail" type="text" icon={FiMail} />
-        <Input placeholder="Senha" type="password" icon={FiLock} />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button title="Entrar" />
+        <Button title="Entrar" onClick={handleSignIn} />
 
         <Link to="/register">Criar conta</Link>
       </Form>
