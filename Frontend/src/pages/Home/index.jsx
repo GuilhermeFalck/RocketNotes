1f8b3ad96f8b3c42e3e22028777f4d14cd423b4f
guilherme <guilhermefalck22@gmail.com>
2024-09-03 10:52:25 -0300

    finished page Home

--- Frontend/src/pages/Home/index.jsx
+++ Frontend/src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { FiPlus, FiSearch } from "react-icons/fi";
 
 import { api } from "../../services/api";
@@ -17,7 +18,13 @@ export function Home() {
   const [tagsSelected, setTagsSelected] = useState([]);
   const [notes, setNotes] = useState([]);
 
+  const navigate = useNavigate();
+
   function handleTagSelected(tagName) {
+    if (tagName === "all") {
+      return setTagsSelected([]);
+    }
+
     const alreadySelected = tagsSelected.includes(tagName);
 
     if (alreadySelected) {
@@ -26,6 +33,10 @@ export function Home() {
     } else {
       setTagsSelected((prevState) => [...prevState, tagName]);
     }
+  }
+
+  function handleDetails(id) {
+    navigate(`/details/${id}`);
   }
 
   useEffect(() => {
@@ -79,7 +90,7 @@ export function Home() {
       <Search>
         <Input
           placeholder="Pesquisar pelo título"
-          onChange={() => setSearch(e.target.value)}
+          onChange={(e) => setSearch(e.target.value)}
           icon={FiSearch}
         />
       </Search>
@@ -87,7 +98,11 @@ export function Home() {
       <Content>
         <Section title="Minhas Notas">
           {notes.map((note) => (
-            <Note key={String(note.id)} data={note} />
+            <Note
+              key={String(note.id)}
+              data={note}
+              onClick={() => handleDetails(note.id)}
+            />
           ))}
         </Section>
       </Content>
